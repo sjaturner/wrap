9357c821f6a1eeeebfdd4b062ccfe077b6e0d162
sjaturner <sjaturner@googlemail.com>
2025-03-16 14:21:46 +0000

    getting there

--- wrap.py
+++ wrap.py
@@ -11,7 +11,7 @@ for statement in statements:
         bracket = [chunk.strip() for chunk in re.split(r'(\([^\)]*\))', statement) if len(chunk.strip())]
         identifier = bracket[0].split()[1]
         items=[item.strip() for item in bracket[1][1:-1].split(',') if len(item.strip())]
-        print(identifier, items);
+#       print(identifier, items);
         call={'name':identifier, 'items':[]}
         for item in items:
             m = re.match(r'char\s*\*\s*(.*)', item)
@@ -30,32 +30,35 @@ for enum in enums:
     print("static char *value_of_enum_%s(char *str) {"%enum['name']);
     print("    if(0) {}")
     for tag, value in enum['items']:
-        print("    else if (!strcmp(str, \"%s\") return %s;"%(tag, value))
+        print("    else if (!strcmp(str, \"%s\")) return %s;"%(tag, value))
     print("    else return 0;")
     print("}")
 
 print("int handle_argc_argv(int argc, char *argv[]) {")
 print("    if(0) {}")
 for function in functions:
-    print("    else if (!strcmp(argv[0], \"%s\") {"%(function['name']))
+    print("    else if (!strcmp(argv[0], \"%s\")) {"%(function['name']))
     for index, type_name in enumerate(function['items']):
         if type_name[1][-1]==']':
             if index + 1 != len(function['items']):
                 print("array can only be last element in list")
                 sys.exit(-1)
-            type_name[0] = type_name[0].split('[')[0]
+            type_name[1] = type_name[1].split('[')[0]
             print("        // array %s %s %d %d"%(type_name[0], type_name[1], index, len(function['items'])))
         else:
             print("        %s %s = parse_%s(argv[%d]);"%(type_name[0], type_name[1], type_name[0], index + 1))
     print("        %s("%function['name'],end='')
     for index, type_name in enumerate(function['items']):
+        if type_name[1][-1]==']':
+            if index + 1 != len(function['items']):
+                print("array can only be last element in list")
+                sys.exit(-1)
+            type_name[1] = type_name[1].split('[')[0]
         print(type_name[1],end='')
         if index + 1 != len(function['items']):
             print(",",end='')
     print(");")
 
     print("    }")
-print("return -1;")
+print("    return -1;")
 print("}")
-
-print(functions)
