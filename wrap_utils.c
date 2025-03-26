05acbf44ec46f19afd4398f445c8099e37b34ddb
sjaturner <sjaturner@googlemail.com>
2025-03-26 19:47:35 +0000

    split strings to argc, argv

---
+++ wrap_utils.c
@@ -0,0 +1,85 @@
+#include "wrap_utils.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int string_to_args(char *str, int limit, char *argv[])
+{
+    if (strchr(str, '%'))
+    {
+        return -1;
+    }
+
+    int len = strlen(str) + 1;
+    char *buf = memset(valloc(len), 0, len);
+    snprintf(buf, len, str);
+    memcpy(str, buf, len);
+
+    char *scan = str;
+    int in_quote = *scan == '"';
+    int last_space = 1;
+    int argc = 0;
+
+    for (; *scan; ++scan)
+    {
+        int this_space = isspace(*scan);
+        if (in_quote)
+        {
+            if (*scan == '"')
+            {
+                in_quote = 0;
+            }
+        } 
+        else
+        {
+            if (last_space)
+            {
+                if (!this_space)
+                {
+                    if (argc >= limit)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        argv[argc++] = scan;
+                    }
+                }
+            }
+            *scan = this_space ? 0 : *scan;
+        }
+
+        last_space = this_space;
+    }
+
+    for (int index = 0; index < argc; ++index)
+    {
+        if (argv[index][0] == '"')
+        {
+            argv[index][0] = 0;
+            ++argv[index];
+        }
+        int length = strlen(argv[index]);
+        if (length && argv[index][length - 1] == '"')
+        {
+            argv[index][length - 1] = 0;
+        }
+    }
+
+    return argc;
+}
+
+int main(int argc, char *argv[])
+{
+    char *dup = strdup("simon \\\\ \"pi\"eman\" \x30");
+
+    char *av[10] = { };
+    int ac = string_to_args(dup, 10, av);
+
+    for (int index = 0; index < ac; ++index)
+    {
+        printf("|%s|\n", av[index]);
+    }
+    return 0;
+}
