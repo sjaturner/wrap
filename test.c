babd0e157ed623261d01335fdc890ad557e49707
sjaturner <sjaturner@googlemail.com>
2025-04-06 14:12:25 +0100

    flesh out the test functions

--- test.c
+++ test.c
@@ -1,7 +1,45 @@
 #include "wrap_utils.h"
+#include "wrap.h"
 
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
+
+
+int first_function(int64_t foo, uint64_t bar)
+{
+    printf("%s %" PRId64 "%" PRIx64 "\n", __func__, foo, bar);
+    return 0;
+}
+
+int next_function(int64_t foo, uint64_t bar)
+{
+    printf("%s %" PRId64 "%" PRIx64 "\n", __func__, foo, bar);
+    return 0;
+}
+
+int stringy_bob(int64_t foo, uint64_t bar, char *str)
+{
+    printf("%s %" PRId64 "%" PRIx64 "\%s\"\n", __func__, foo, bar, str);
+    return 0;
+}
+
+int another_function(uint64_t self, char *blah, an_enum an_enum)
+{
+    printf("%s %" PRIx64 "\%s\" %d\n", __func__, self, blah, an_enum);
+    return 0;
+}
+
+int pass_through(uint64_t self, int argc, char *argv[])
+{
+    printf("%s %" PRIx64 " ", __func__, self);
+    while (argc--)
+    {
+        printf("%s ", *argv++);
+    }
+    printf("\n");
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
