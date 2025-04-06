96fdb4121d9c000f92a9907228ea0b9b9a19501e
sjaturner <sjaturner@googlemail.com>
2025-04-06 13:56:28 +0100

    will test like this for a start

--- test.c
+++ test.c
@@ -1,16 +1,27 @@
 #include "wrap_utils.h"
 
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[])
 {
-    char *dup = strdup("simon \\\\ \"pi\"eman\" \x30");
+    if (argc < 2)
+    {
+        return 0;
+    }
+
+    --argc, ++argv;
+
+    char cmd[0x10000] = { };
+
+    while (argc--)
+    {
+        strcat(cmd, *argv++);
+        strcat(cmd, " ");
+    }
 
     char *av[10] = { };
-    int ac = string_to_args(dup, 10, av);
+    int ac = string_to_args(cmd, 10, av);
 
     for (int index = 0; index < ac; ++index)
     {
